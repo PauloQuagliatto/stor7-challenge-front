a000d7833e78216b1abcad2279107866aafc8f19
PauloQuagliatto <pquagliatto17@gmail.com>
2022-02-10 22:28:38 -0300

    redirect url now is an env variable

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -5,7 +5,7 @@ const LoginPage = () => {
     <Container>
       <h1>Login</h1>
       <a
-        href={`https://accounts.zoho.com/oauth/v2/auth?scope=ZohoCRM.modules.contacts.ALL&client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=code&access_type=online&redirect_uri=http://localhost:5000/redirect`}
+        href={`https://accounts.zoho.com/oauth/v2/auth?scope=ZohoCRM.modules.contacts.ALL&client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=code&access_type=online&redirect_uri=${REACT_APP_REDIRECT_URL}`}
       >
         Logar com Zoho
       </a>
